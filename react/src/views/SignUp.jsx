275371851336c93f0963d1d746c451968b039366
Bouchtarate <hamza.bouchtarate@gmail.com>
2023-07-10 21:54:53 +0100

    add axiosclient request & response message

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,17 +1,49 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 export default function SignUp() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmationRef = useRef();
+  const { setUser, setToken } = useStateContext();
   const onSubmit = (ev) => {
     ev.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    };
+    console.log(payload);
+
+    axiosClient
+      .post("/signup", payload)
+      .then(({ data }) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          console.log(response.data.errors);
+        }
+      });
   };
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form action="" onSubmit={onSubmit}>
           <h1 className="title">Sign up for free</h1>
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Password Confirmation" />
+          <input ref={nameRef} type="text" placeholder="Full Name" />
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <input
+            ref={passwordConfirmationRef}
+            type="password"
+            placeholder="Password Confirmation"
+          />
           <button className="btn btn-block">Sign up</button>
           <p className="message">
             Already Register?<Link to="/login">Login</Link>
